aa638a9d994ea6095e8a391bca973fd7e1ce6188
brian d foy <brian.d.foy@gmail.com>
2005-03-12 05:55:17 +0000

    * Added POD coverage tests, fixed issues * Updated copyright dates
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@1623 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- lib/Safari.pm
+++ lib/Safari.pm
@@ -37,7 +37,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT
 
-Copyright 2003-2004, brian d foy, All rights reserved
+Copyright 2003-2005, brian d foy, All rights reserved
 
 This library is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
