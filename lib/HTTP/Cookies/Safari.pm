e82cba891af821b81b6457166bb26c7b75eefef7
brian d foy <brian.d.foy@gmail.com>
2011-04-18 14:39:09 +0200

    Update the copyright

--- lib/HTTP/Cookies/Safari.pm
+++ lib/HTTP/Cookies/Safari.pm
@@ -45,7 +45,7 @@ Jon Orwant pointed out the problem with dates too far in the future
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2003-2009 brian d foy.  All rights reserved.
+Copyright (c) 2003-2011 brian d foy.  All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
